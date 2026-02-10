bf7d3f5e6c61eb2482dc9f70ac8d7040f927fdab
Dhodduraaj <dhodduraajsp@gmail.com>
2026-02-10 11:25:11 +0530

    still fixing

---
+++ dogvaathi-backend/src/main/java/com/example/dogvaathi/DogvaathiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogvaathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogvaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogvaathiApplication.class, args);
+	}
+
+}
---
+++ dogvaathi-backend/src/test/java/com/example/dogvaathi/DogvaathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogvaathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogvaathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
